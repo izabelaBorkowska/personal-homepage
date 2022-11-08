96253dfcc8e156391caef2377c7ebffc67aa6b85
izabelaBorkowska <117465630+izabelaBorkowska@users.noreply.github.com>
2022-11-08 10:44:33 +0100

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+console.log("Witaj na mojej stronie!-Zmiana wiadomosci");
+
+// JS -zmiana motywu
+
+let changeTheme = document.querySelector(".changeTheme");
+let body = document.querySelector(".body");
+ 
+changeTheme.addEventListener("click", () => {
+body.classList.toggle("dark");
+
+changeTheme.innerText = body.classList.contains("dark") ? "Włącz jasny motyw" : "Włącz ciemny motyw"
+});
+
+//JS poprzednia - moja wersja na dole 
+
+//  if (body.classList.contains("dark")) {
+ //  changeTheme.innerText = "Włącz jasny motyw";
+ //} else {
+  //  changeTheme.innerText = "Włącz ciemny motyw";
+//}
+
+// JS -usuniecie zdjecia
+
+let buttonPhoto = document.querySelector(".buttonPhoto");
+
+buttonPhoto.addEventListener("click", () => {
+  let mainPhoto = document.querySelector(".mainPhoto");
+  mainPhoto.remove();
+});
