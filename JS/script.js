4eb907a5de7e442775d713b348acc99eba09fe56
izabelaBorkowska <117465630+izabelaBorkowska@users.noreply.github.com>
2022-11-11 12:27:29 +0100

    changed some of class names (hw4)

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ console.log("Witaj na mojej stronie!-Zmiana wiadomosci");
 
 // JS -zmiana motywu
 
-let changeTheme = document.querySelector(".changeTheme");
+let changeTheme = document.querySelector(".js-changeTheme");
 let body = document.querySelector(".body");
  
 changeTheme.addEventListener("click", () => {
@@ -21,7 +21,7 @@ changeTheme.innerText = body.classList.contains("dark") ? "Włącz jasny motyw" 
 
 // JS -usuniecie zdjecia
 
-let buttonPhoto = document.querySelector(".buttonPhoto");
+let buttonPhoto = document.querySelector(".js-buttonPhoto");
 
 buttonPhoto.addEventListener("click", () => {
   let mainPhoto = document.querySelector(".mainPhoto");
